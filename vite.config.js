8227aea4b551b09f1a726f9eb28e77fc868ad090
Rodrigoruiz12 <rod.ruiz@duocuc.cl>
2026-05-10 01:10:04 -0400

    cambio vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api/v1/ventas': {
-        target: 'http://10.0.132.216:8080', // Puerto de Ventas
+        target: 'http://10.0.132.216:8080',
         changeOrigin: true,
       },
       '/api/v1/despachos': {
-        target: 'http://10.0.132.216:8081', // Puerto de Despachos
+        target: 'http://10.0.132.216:8081',
         changeOrigin: true,
       }
     }
